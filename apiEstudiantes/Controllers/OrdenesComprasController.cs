d4d9c3f958c493b2db5bdc3f040c866af77d9746
Jose Santos Toribio <josesantostoribio@gmail.com>
2021-04-12 08:22:36 -0400

    Modificación del controlador de Ordenes de compras

--- apiEstudiantes/Controllers/OrdenesComprasController.cs
+++ apiEstudiantes/Controllers/OrdenesComprasController.cs
@@ -2,46 +2,116 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using apiCompras.Context;
+using apiCompras.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace apiCompras.Controllers
 {
     [Route("api/[controller]")]
-    [ApiController]
-    public class OrdenesComprasController : ControllerBase
+    //[ApiController]
+    public class OrdenesComprasController : Controller
     {
+        private readonly AppDbContext context;
+        public OrdenesComprasController(AppDbContext context)
+        {
+            this.context = context;
+        }
+
         // GET: api/<OrdenesComprasController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public ActionResult Get()
         {
-            return new string[] { "value1", "value2" };
+            try
+            {
+                return Ok(context.Orden_Compra.ToList());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         // GET api/<OrdenesComprasController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public ActionResult Get(int id)
         {
-            return "value";
+            try
+            {
+                var gestor = context.Orden_Compra.FirstOrDefault(g => g.Id_Orden_Compra == id);
+                return Ok(gestor);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         // POST api/<OrdenesComprasController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public ActionResult Post([FromBody] Orden_Compra_Bd gestor)
         {
+            try
+            {
+                context.Orden_Compra.Add(gestor);
+                context.SaveChanges();
+                return CreatedAtRoute("GetGestor", new { id = gestor.Id_Orden_Compra }, gestor);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         // PUT api/<OrdenesComprasController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public ActionResult Put(int id, [FromBody] Orden_Compra_Bd gestor)
         {
+            try
+            {
+                if (gestor.Id_Orden_Compra == id)
+                {
+                    context.Entry(gestor).State = EntityState.Modified;
+                    context.SaveChanges();
+                    return CreatedAtRoute("GetGestor", new { id = gestor.Id_Orden_Compra }, gestor);
+
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         // DELETE api/<OrdenesComprasController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public ActionResult Delete(int id)
         {
+            try
+            {
+                var gestor = context.Orden_Compra.FirstOrDefault(g => g.Id_Orden_Compra == id);
+                if (gestor != null)
+                {
+                    context.Orden_Compra.Remove(gestor);
+                    context.SaveChanges();
+                    return Ok(id);
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
